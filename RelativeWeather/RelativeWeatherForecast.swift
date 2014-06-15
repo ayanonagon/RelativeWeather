bd4fd45bdd85f6ba10175c8dcbd03df9bc245b4e
Ayaka Nonaka <ayanonagon@gmail.com>
2014-06-14 21:53:47 -0700

    Taylor Swift uses 4 spaces.

--- RelativeWeather/RelativeWeatherForecast.swift
+++ RelativeWeather/RelativeWeatherForecast.swift
@@ -10,15 +10,15 @@ import Foundation
 
 class RelativeWeatherForecast {
 
-  var currentCondition : String?
-  var tomorrowCondition : String?
+    var currentCondition : String?
+    var tomorrowCondition : String?
 
-  var yesterdayHigh : Float = 0
-  var todayHigh : Float = 0
-  var tomorrowHigh : Float = 0
+    var yesterdayHigh : Float = 0
+    var todayHigh : Float = 0
+    var tomorrowHigh : Float = 0
 
-  var yesterdayLow : Float = 0
-  var todayLow : Float = 0
-  var tomorrowLow : Float = 0
+    var yesterdayLow : Float = 0
+    var todayLow : Float = 0
+    var tomorrowLow : Float = 0
 
 }
--- RelativeWeatherTests/RelativeWeatherForecastTests.swift
+++ RelativeWeatherTests/RelativeWeatherForecastTests.swift
@@ -11,18 +11,18 @@ import RelativeWeather
 
 class RelativeWeatherForecastTests : XCTestCase {
 
-  func testInit() {
-    let forecast : RelativeWeatherForecast = RelativeWeatherForecast()
-    XCTAssertNil(forecast.currentCondition)
-    XCTAssertNil(forecast.tomorrowCondition)
+    func testInit() {
+        let forecast : RelativeWeatherForecast = RelativeWeatherForecast()
+        XCTAssertNil(forecast.currentCondition)
+        XCTAssertNil(forecast.tomorrowCondition)
 
-    XCTAssertEqualWithAccuracy(forecast.yesterdayHigh, 0.0, 0.001)
-    XCTAssertEqualWithAccuracy(forecast.todayHigh, 0.0, 0.001)
-    XCTAssertEqualWithAccuracy(forecast.tomorrowHigh, 0.0, 0.001)
+        XCTAssertEqualWithAccuracy(forecast.yesterdayHigh, 0.0, 0.001)
+        XCTAssertEqualWithAccuracy(forecast.todayHigh, 0.0, 0.001)
+        XCTAssertEqualWithAccuracy(forecast.tomorrowHigh, 0.0, 0.001)
 
-    XCTAssertEqualWithAccuracy(forecast.yesterdayLow, 0.0, 0.001)
-    XCTAssertEqualWithAccuracy(forecast.todayLow, 0.0, 0.001)
-    XCTAssertEqualWithAccuracy(forecast.tomorrowLow, 0.0, 0.001)
-  }
+        XCTAssertEqualWithAccuracy(forecast.yesterdayLow, 0.0, 0.001)
+        XCTAssertEqualWithAccuracy(forecast.todayLow, 0.0, 0.001)
+        XCTAssertEqualWithAccuracy(forecast.tomorrowLow, 0.0, 0.001)
+    }
 
 }
